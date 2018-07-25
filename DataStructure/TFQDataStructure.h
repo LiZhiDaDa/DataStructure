4e9abc93f90d061bea636144efc461c22730adce
王立志 <1226169349@qq.com>
2018-07-25 17:45:36 +0800

    add C file

---
+++ DataStructure/TFQDataStructure.h
@@ -0,0 +1,15 @@
+//
+//  TFQDataStructure.h
+//  DataStructure
+//
+//  Created by 王立志 on 2018/7/25.
+//  Copyright © 2018年 Thread_Fight_Queue. All rights reserved.
+//
+
+#ifndef TFQDataStructure_h
+#define TFQDataStructure_h
+
+#include <stdio.h>
+void test(void);
+
+#endif /* TFQDataStructure_h */
---
+++ DataStructure/main.c
@@ -0,0 +1,20 @@
+//
+//  main.c
+//  DataStructure
+//
+//  Created by 王立志 on 2018/7/25.
+//  Copyright © 2018年 Thread_Fight_Queue. All rights reserved.
+//
+
+#include <stdio.h>
+#include "TFQDataStructure.h"
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    
+    test();
+    return 0;
+}
+
+
