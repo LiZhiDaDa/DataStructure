4093fd96a8c1c2ed6e721480c30c8bf3fb97a10a
王立志 <1226169349@qq.com>
2018-07-25 15:25:50 +0800

    Create C File

---
+++ DataStructure/DataStructure/TFQDataStructure.c
@@ -0,0 +1,9 @@
+//
+//  TFQDataStructure.c
+//  DataStructure
+//
+//  Created by 王立志 on 2018/7/25.
+//  Copyright © 2018年 Thread_Fight_Queue. All rights reserved.
+//
+
+#include "TFQDataStructure.h"
---
+++ DataStructure/DataStructure/TFQDataStructure.h
@@ -0,0 +1,14 @@
+//
+//  TFQDataStructure.h
+//  DataStructure
+//
+//  Created by 王立志 on 2018/7/25.
+//  Copyright © 2018年 Thread_Fight_Queue. All rights reserved.
+//
+
+#ifndef TFQDataStructure_h
+#define TFQDataStructure_h
+
+#include <stdio.h>
+
+#endif /* TFQDataStructure_h */
