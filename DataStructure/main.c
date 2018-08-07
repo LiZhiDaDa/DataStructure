62f6f85066ce0284df74710e63752b01cd5600e3
王立志 <1226169349@qq.com>
2018-08-07 18:41:01 +0800

    change structure

--- DataStructure/main.c
+++ DataStructure/main.c
@@ -9,120 +9,16 @@
 #include <stdio.h>
 #include "TFQDataStructure.h"
 
-//定义String结构体
-#define MAXSIZE 20
-typedef struct{
-    char *ch;
-    int len;
-}String;
-int subStringIndex(String S, String T, int pos);
-void calculation(void);
-
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    //    printf("Hello, World!\n");
-    //    test();
-    calculation();
     
-    return 20;
-}
-
-#pragma mark - 二叉树
-typedef int TElemType;
-typedef struct BiTnode{
-    TElemType data;
-    struct BiTnode *lchild, *rchild;
-}BiTnode, *BiTree;
-///前序遍历二叉树
-void preorderTraversal(BiTree T){
-    if(T == NULL){
-        return;
-    }
-    printf("%c",T->data);
-    preorderTraversal(T->lchild);
-    preorderTraversal(T->rchild);
-}
-///中序遍历二叉树
-void inorderTraversal(BiTree T){
-    if(T == NULL){
-        return;
-    }
-    preorderTraversal(T->lchild);
-    printf("%c",T->data);
-    preorderTraversal(T->rchild);
-}
-///后序遍历二叉树
-void postorderTraversal(BiTree T){
-    if(T == NULL){
-        return;
-    }
-    preorderTraversal(T->lchild);
-    preorderTraversal(T->rchild);
-    printf("%c",T->data);
-}
-
-#pragma mark - 串
-/**
- *  模式匹配算法：子串的定位操作通常称做串的模式匹配
- *  假如要从S="goodgoogle" 中找到T="google"这个子串的位置
- *  S[0]、T[0]分别为两个串的长度。
- *  返回子串T在主串S中第pos个字符之后的位置。若不存在返回0
- *  T非空， 1<= pos <=StrLength(S)
- */
-void calculation(){
-    String S,T;
-    S.ch = "goodgoogle";
-    T.ch = "google";
-    S.len = 10;
-    T.len = 6;
-    int result = subStringIndex(S, T, 0);
-    printf("%d\n\n", result);
-}
-
-/// 一般的查找子串算法 时间复杂度为O(n)
-int subStringIndex(String S, String T, int pos){
-    int i = pos;
-    int j = 0;
-    while(i<S.len && j<T.len){
-        if(S.ch[i] == T.ch[j]){
-            i++;
-            j++;
-        }else{
-            i=i-j+1;
-            j=0;
-        }
-    }
-    if(j == T.len){
-        return i-T.len;
-    }else{
-        return 0;
-    }
-}
-
-/// KMC模式匹配算法需要的next数组，KMC的重点就是理解next数组是怎么来的
-void getNext(String T, int *next){
-    int i=1;
-    int j=0;
-    next[0]=0;
-    while (i<T.len) {
-        if(T.ch[i] == T.ch[j]){
-            i++;
-            j++;
-            next[j] = j;
-        }else{
-            j = next[j];
-        }
-    }
-}
-
-/// KMC模式匹配算法
-int KMCSubStringIndex(String S, String T, int pos){
     return 0;
 }
 
-
-
-
+/**
+ *  项目架构参考文章中的思维导图：
+ *  《大话数据结构》https://juejin.im/post/5b6976a2f265da0f61322df6
+ */
+ 
 
 
 
