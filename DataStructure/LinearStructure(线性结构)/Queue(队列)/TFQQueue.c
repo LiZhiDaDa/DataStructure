f54506a196c9dcc11b0cd9f6117a61256574fe39
王立志 <1226169349@qq.com>
2018-08-10 14:10:33 +0800

    calculate queue length

--- DataStructure/LinearStructure(线性结构)/Queue(队列)/TFQQueue.c
+++ DataStructure/LinearStructure(线性结构)/Queue(队列)/TFQQueue.c
@@ -9,6 +9,9 @@
 #include "TFQQueue.h"
 
 typedef int ElementType;
+typedef int Status;
+#define ERROR 0
+#define OK 1
 #define MAXSIZE 20
 struct queue{
     ElementType data[MAXSIZE];
@@ -27,8 +30,50 @@ typedef struct queue Queue;
  *  空队列时，front=rear，但是队列满时front也=rear，那怎么判断队列是空还是满呢？
  *  方法一：设置flag，当front=rear，且flag=0，队列为空；当front==rear，且flag=1，队列满。
  *  方法二：front=rear队列空，当队列满时我们保留一个元素空间，这样就好判断了。
+ *  下面介绍的就是第二种方法。
  */
-
-
+//初始化一个空队列
+Status initQueue(Queue *q){
+    q->front = 0;
+    q->rear = 0;
+    return OK;
+}
+/**
+ *  循环队列长度  这个可以推倒，一种情况是rear>front len=rear-front
+ *  另一种情况是rear<front,一部分长度是rear另一部分长度是maxsize-front ==> rear+maxsize-front
+ *  这样可以推倒出通用长度公式就是下边的部分。
+ */
+int queueLength(Queue q){
+    return (q.rear - q.front+MAXSIZE)%MAXSIZE;
+}
 
 #pragma mark - -----队列的链式存储结构-----
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
